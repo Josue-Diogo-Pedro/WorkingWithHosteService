7061a2a2e0582b6cb42f8d836e69f9d6724188d1
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-23 02:11:07 -0800

    Add project files.

---
+++ WorkingWithHostedService/Services/BackgroundService.cs
@@ -0,0 +1,14 @@
+﻿namespace WorkingWithHostedService.Services;
+
+public abstract class BackgroundService : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
