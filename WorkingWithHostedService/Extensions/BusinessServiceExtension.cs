6fd677e7e6dc875f696fed24c52390f0068853ef
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-02-26 02:59:41 -0800

    Finish to implement extension that hable services

--- WorkingWithHostedService/Extensions/BusinessServiceExtension.cs
+++ WorkingWithHostedService/Extensions/BusinessServiceExtension.cs
@@ -1,6 +1,16 @@
-﻿namespace WorkingWithHostedService.Extensions
+﻿using WorkingWithHostedService.Services;
+
+namespace WorkingWithHostedService.Extensions;
+
+public static class BusinessServiceExtension
 {
-    public class BusinessServiceExtension
+    public static void AddBusiness(this IServiceCollection services)
     {
+        services.AddScoped<SampleService>();
+        services.AddSingleton<SettingService>();
+        services.AddSingleton<PeriodicHostedService>();
+        services.AddHostedService(provider =>
+            provider.GetRequiredService<PeriodicHostedService>()
+        );
     }
 }
